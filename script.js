3b09b743199976b3ca694a712269100b32efeed6
Patrick Gross <bears_lair@protonmail.com>
2024-10-14 16:49:30 -0700

    book constructor created

--- script.js
+++ script.js
@@ -4,27 +4,24 @@ const dialog = document.querySelector("dialog");
 const closeModal = document.querySelector("dialog #closeModal");
 
 const myLibrary = [
-  {
-    id: 0,
-    title: "Book 1",
-    author: "Patrick",
-    pages: 256,
-    "was-read": true,
-  },
-  {
-    id: 1,
-    title: "Book 2",
-    author: "Melvin",
-    pages: 129,
-    "was-read": false,
-  },
-  {
-    id: 2,
-    title: "Book 3",
-    author: "Beatrice",
-    pages: 92,
-    "was-read": true,
-  },
+  // {
+  //   title: "Book 1",
+  //   author: "Patrick",
+  //   pages: 256,
+  //   "was-read": true,
+  // },
+  // {
+  //   title: "Book 2",
+  //   author: "Melvin",
+  //   pages: 129,
+  //   "was-read": false,
+  // },
+  // {
+  //   title: "Book 3",
+  //   author: "Beatrice",
+  //   pages: 92,
+  //   "was-read": true,
+  // }
 ];
 
 displayBookCollection();
@@ -37,8 +34,11 @@ closeModal.addEventListener("click", () => {
   dialog.close();
 });
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
@@ -71,10 +71,10 @@ function deleteABook(book) {
 }
 
 function wasRead(book) {
-  if (myLibrary[book]["was-read"] === false) {
-    myLibrary[book]["was-read"] = true;
-  } else if (myLibrary[book]["was-read"] === true) {
-    myLibrary[book]["was-read"] = false;
+  if (myLibrary[book].read === false) {
+    myLibrary[book].read = true;
+  } else if (myLibrary[book].read === true) {
+    myLibrary[book].read = false;
   }
 
   displayBookCollection();
