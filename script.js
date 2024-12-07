faa02698b6fc880e6257e4fa66f9d78a30d48c78
Patrick Gross <bears_lair@protonmail.com>
2024-12-06 21:38:48 -0800

    book class added to script

--- script.js
+++ script.js
@@ -49,11 +49,13 @@ function addBookToLibrary() {
 
   console.log("read equals ", read);
 
-  function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  class Book {
+    constructor(title, author, pages, read) {
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+      this.read = read;
+    }
   }
 
   const userBook = new Book(title, author, pages, read);
